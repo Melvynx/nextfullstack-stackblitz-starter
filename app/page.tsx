c83a43cb2458336b547fa5443a9c481aa2c5bd1a
melvynxdev <melvynmal@gmail.com>
2025-01-27 12:38:28 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default async function RoutePage() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Stackblitz start</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+}
